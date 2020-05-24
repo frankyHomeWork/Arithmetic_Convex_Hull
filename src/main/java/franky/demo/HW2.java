b483dd788bc8cb4c3841612b662d9332d52a02f2
franky3020 <210238hwsh@gmail.com>
2020-05-24 13:24:47 +0800

    置中且依據原件大小動態調整

--- src/main/java/franky/demo/HW2.java
+++ src/main/java/franky/demo/HW2.java
@@ -2,7 +2,6 @@ package franky.demo;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
-import javax.swing.border.Border;
 import java.awt.BorderLayout;
 
 public class HW2 {
@@ -10,9 +9,6 @@ public class HW2 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("MyCanvasDemo_1");
         //設定視窗顯示在螢幕在的位置
-        frame.setLocation(100,100);
-        frame.setSize(500,500);
-        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(new BorderLayout());
 
@@ -21,6 +17,9 @@ public class HW2 {
 
         TestCanvas a_canvas = new TestCanvas();
         frame.add(a_canvas, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
 
--- src/main/java/franky/demo/TestCanvas.java
+++ src/main/java/franky/demo/TestCanvas.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class TestCanvas extends Canvas {
     public TestCanvas() {
-        setSize(640,480);
+        setSize(500,500);
     }
     public void paint(Graphics g) {
         //畫一條直線,從座標(100,100)到(150,150)
