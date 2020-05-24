28fc2287128732e1cb5d860d839d6c659f40762f
franky3020 <210238hwsh@gmail.com>
2020-05-24 13:02:22 +0800

    init

---
+++ src/main/java/franky/demo/TestCanvas.java
@@ -0,0 +1,21 @@
+package franky.demo;
+
+import java.awt.*;
+
+public class TestCanvas extends Canvas {
+    public TestCanvas() {
+        setSize(640,480);
+    }
+    public void paint(Graphics g) {
+        //畫一條直線,從座標(100,100)到(150,150)
+        System.out.println("in paint");
+        g.drawLine(0,0,15,15);
+    }
+    public void update(Graphics g) {
+        System.out.println("in Update");
+    }
+}
+
+
+
+
